ae5f76b04c27f9a8f5543fefcb139f5196ad0634
easa <eisanodehi@gmail.com>
2020-04-02 21:27:48 +0000

    Deploying to gh-pages from  @ 37434935e0d3894c4b48534a0ac8c0befc7dd5e0 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ed5fbbe78f528dbe019718e20f152267.js"
+  "/reactboard/precache-manifest.18e4d890bdefc9fb2af60df87d18c398.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/reactboard/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
